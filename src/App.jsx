a73694311be8756f68738d5cea2e767650847f34
deepan-codebuster <112kdeepan@gmail.com>
2024-11-02 11:53:24 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound"
+import NotFound from "./pages/NotFound";
 import AddTrip from "./pages/AddTrip";
 import Profile from "./pages/ProfilePage";
 import Tripinfo from "./pages/TripInfo";
